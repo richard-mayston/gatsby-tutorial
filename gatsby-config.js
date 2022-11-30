1114df5b053d60ff748e318b076e7eb427c2d005
Richard Mayston <richard.mayston@arthaus.co.uk>
2022-11-30 12:56:54 +0000

    added gatsby plugin image

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"]
+  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-image", "gatsby-plugin-sharp"]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import * as React from "react"
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageHeading="Homepage">
       <p>Home intro</p>
+      <StaticImage src="../images/icon.png" placeholder="blurred"
+        layout="fixed"
+        width={200}
+        height={200} alt="logo" />
+      <StaticImage src="https://s3.amazonaws.com/com.mirrorproject.photos/2001/12/01/3040-15065.jpg" alt="Me" />
     </Layout>
   )
 };
